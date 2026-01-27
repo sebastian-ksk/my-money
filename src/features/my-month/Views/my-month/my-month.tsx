f93d17e1b12df2b179976bf9c6fca60b780b4adb
sebastian-ksk <57274306+sebastian-ksk@users.noreply.github.com>
2026-01-26 22:13:19 -0500

    Update MyMonth component to include additional padding in the container for improved layout

--- src/features/my-month/Views/my-month/my-month.tsx
+++ src/features/my-month/Views/my-month/my-month.tsx
@@ -433,7 +433,7 @@ const MyMonth = () => {
   );
 
   return (
-    <div className='container mx-auto  sm:px-4 py-2 sm:py-4 '>
+    <div className='container mx-auto  sm:px-4 py-2 sm:py-4 px-2 '>
       {/* Onboarding Tour */}
       <AppOnboarding
         page='my-month'
